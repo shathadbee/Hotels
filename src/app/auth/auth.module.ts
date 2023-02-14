5ed9f5601ec55f0d07231ddbcb387b001261d04b
shathadbe <shathaaldbee@gmail.com>
2023-02-14 22:07:48 +0200

    header

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
-    AuthComponent
+    AuthComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     CommonModule,
