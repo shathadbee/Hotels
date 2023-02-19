d1db97a61c241c46bd5edb0e4c07f4ca06c4ee57
IzzeldeenAsh <ezraashour00@gmail.com>
2023-02-19 10:10:06 +0300

    AHAD

--- src/app/pages/home/hotels/hotels.component.ts
+++ src/app/pages/home/hotels/hotels.component.ts
@@ -5,74 +5,49 @@ import { HotelsAPIService } from 'src/app/services/hotels-api.service';
 @Component({
   selector: 'app-hotels',
   templateUrl: './hotels.component.html',
-  styleUrls: ['./hotels.component.scss']
+  styleUrls: ['./hotels.component.scss'],
 })
-export class HotelsComponent  implements OnInit{
-cities:any[]=[];
-  dataArray :any[]=[];
-  current:any;
-  constructor(private  hotelsAPI: HotelsAPIService){ }
+export class HotelsComponent implements OnInit {
+  cities: any[] = [];
+  dataArray: any[] = [];
+  current: any;
+  constructor(private hotelsAPI: HotelsAPIService) {}
 
   ngOnInit(): void {
-this.getAlHotels();
-
+    this.getAlHotels();
+    this.getCities();
   }
-getAlHotels(){
-  this.hotelsAPI.getHotels().subscribe((result:any) =>{
-
-    if (result){
-      this.dataArray=result;
-
-for(let x=0;x<=this.dataArray.length;x++){
-
-  if(this.cities.includes(this.dataArray[x].city)){
-continue;
-
-
-  }
-  else{
-    this.cities.push(this.dataArray[x].city)
-  }
-
-}
-
-        }
-  }
-
-//get cities
-
-
-
-  )
-
-}
-tabChanged($event:any){
-
-  if (this.cities) {
-    this.current = [];
-    this.dataArray.forEach((hotel:IHotels) => {
-      let city_Name = hotel.city;
-      console.log($event);
-      if (city_Name.includes($event.tab.textLabel as never)) {
-        this.current.push(hotel);
-        console.log(this.current);
+  getAlHotels() {
+    this.hotelsAPI.getHotels().subscribe((result: any) => {
+      if (result) {
+        this.dataArray = result;
       }
     });
-
+  }
+  //sds
+  getCities() {
+    this.hotelsAPI.getHotels().subscribe((result: any) => {
+      this.cities = [];
+      for (let x = 0; x < result.length; x++) {
+        if (this.cities.includes(result[x].city)) {
+          continue;
+        } else {
+          this.cities.push(result[x].city);
+        }
+      }
+    });
+  }
+  tabChanged($event: any) {
+    if (this.cities) {
+      this.current = [];
+      this.dataArray.forEach((hotel: IHotels) => {
+        let city_Name = hotel.city;
+        console.log($event);
+        if (city_Name.includes($event.tab.textLabel as never)) {
+          this.current.push(hotel);
+          console.log(this.current);
+        }
+      });
+    }
   }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
