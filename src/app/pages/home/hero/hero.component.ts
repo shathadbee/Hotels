855a28932dd150b6830327ff4aee5b1fda82a9ba
= <Izzeldeen.Ashour@b12app.com>
2023-02-22 21:57:07 +0300

    Pics

--- src/app/pages/home/hero/hero.component.ts
+++ src/app/pages/home/hero/hero.component.ts
@@ -1,47 +1,45 @@
 import { Component } from '@angular/core';
-import SwiperCore, { Autoplay,EffectFade } from "swiper";
+import SwiperCore, { Autoplay, EffectFade } from 'swiper';
 
-SwiperCore.use([Autoplay,EffectFade]);
+SwiperCore.use([Autoplay, EffectFade]);
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent {
   sliderNavData = [
     {
       id: 1,
-      img: 'assets/img/slider/nav/slider-nav-4.jpg',
-      title: "6 Courses",
-      subtitle: "Programming Languages",
-      bgColor: "orange-bg",
+      img: 'https://i.ibb.co/svVv3Qt/oryx-hotel-aqaba.jpg',
+      title: '',
+      subtitle: 'Oryx',
+      bgColor: 'orange-bg',
     },
     {
       id: 2,
-      img: 'assets/img/slider/nav/slider-nav-1.jpg',
-      title: "4 Courses",
-      subtitle: "Idea Discussion",
-      bgColor: "blue-bg",
+      img: 'https://i.ibb.co/tmhHxZZ/Screenshot-2023-02-18-at-8-32-37-PM.png',
+      title: '',
+      subtitle: 'Mövenpick',
+      bgColor: 'blue-bg',
     },
     {
       id: 3,
-      img: 'assets/img/slider/nav/slider-nav-2.jpg',
-      title: "8 Courses",
-      subtitle: "Web Development",
-      bgColor: "pink-bg",
+      img: 'https://storage.kempinski.com/cdn-cgi/image/w=480,h=550,f=auto,g=auto,fit=cover/ki-cms-prod/images/0/5/9/1/311950-1-eng-GB/10ed85706c3f-74342834_4K.jpg',
+      title: '',
+      subtitle: 'KEMPINSKI',
+      bgColor: 'pink-bg',
     },
     {
       id: 4,
-      img: 'assets/img/slider/nav/slider-nav-3.jpg',
-      title: "9 Courses",
-      subtitle: "System Administration",
-      bgColor: "green-bg",
+      img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/68/45/8a/exterior.jpg?w=1100&h=-1&s=1',
+      title: '',
+      subtitle: 'Hilton',
+      bgColor: 'green-bg',
     },
-  ]
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
