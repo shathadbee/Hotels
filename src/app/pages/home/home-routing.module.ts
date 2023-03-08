d92df406a54526ae5bd98dbbb3d549915f65e53c
shathadbee <shathadbee@gmail.com>
2023-03-08 13:44:37 -0800

    new3

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutAreaComponent } from './about-area/about-area.component';
 import { HomeComponent } from './home/home.component';
+import { HotelsComponent } from './hotels/hotels.component';
 
 const routes: Routes = [
   {path:'',
@@ -9,6 +11,11 @@ const routes: Routes = [
 
   {path: 'home',
   component: HomeComponent}
+  ,
+  {path: 'hotels',
+  component: HotelsComponent},
+  {path: 'about-area',
+  component: AboutAreaComponent }
 ];
 
 @NgModule({
